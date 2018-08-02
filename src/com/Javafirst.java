9a28d1ed9324023315dd4eb358e7dd5b9eac9942
1314520yzj <1552438300@qq.com>
2018-08-02 14:00:47 +0800

    creat project

---
+++ src/com/Javafirst.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Javafirst{
+
+    public static void main(String[] args){
+
+        System.out.println("我成功配置啦java linux 环境!");
+        System.out.println("我成功下载了idea!");
+
+    }
+
+}
